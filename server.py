b86d8e0d96f48f6d90893e870e1f5ea392267b88
Maxi <maximilian.lewald@web.de>
2021-12-10 21:57:29 +0100

    Add MQTT functionality

--- server.py
+++ server.py
@@ -1,17 +1,16 @@
 from flask import Flask, render_template, request
-#import paho.mqtt.publish as publish
+import paho.mqtt.publish as publish
 
-#MQTT_SERVER = "192.168.178.58"
-#MQTT_PATH = "/growtent/led/red"
+MQTT_SERVER = "192.168.178.58"
+MQTT_PATH = "/growtent/led/red"
 
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
-        if request.method == 'POST':
-#                publish.single(MQTT_PATH, "hello", hostname=MQTT_SERVER)
-                print("Hello")
-#       return render_template('index.html')
-        return render_template('index.html')
+    if request.method == 'POST':
+        publish.single(MQTT_PATH, request.form['sunrise'], hostname=MQTT_SERVER)
+#       publish.single(MQTT_PATH, "hello", hostname=MQTT_SERVER)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
