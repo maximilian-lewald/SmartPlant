5ff6ca0c4da3216afb883eaa7fbc728379bb7522
maximilian-lewald <maximilian@lewald.bayern>
2021-12-14 13:29:58 +0100

    Feat dynamic html

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import paho.mqtt.publish as publish, json
 MQTT_SERVER = "192.168.178.58"
 MQTT_PATH = "/growtent/led/red"
 lampValues = {}
+element = hello
 
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
@@ -13,7 +14,7 @@ def index():
         lampValues['lamp'] = {'sunrise': request.form['sunrise'], 'sunset': request.form['sunset'], 'white': request.form['white'], 'red': request.form['red'], 'blue': request.form['blue']}
         with open('/home/pi/SmartPlant/lampValues.json', 'w') as f:
             json.dump(lampValues, f)
-    return render_template('index.html')
+    return render_template('index.html', element)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
