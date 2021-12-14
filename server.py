aa5ca9bf5e5dcca5f5af26286f6be409455fdda5
maximilian-lewald <maximilian@lewald.bayern>
2021-12-14 13:56:40 +0100

    Fix bug

--- server.py
+++ server.py
@@ -13,8 +13,6 @@ def index():
         lampValues['lamp'] = {'sunrise': request.form['sunrise'], 'sunset': request.form['sunset'], 'white': request.form['white'], 'red': request.form['red'], 'blue': request.form['blue']}
         with open('/home/pi/SmartPlant/lampValues.json', 'w') as f:
             json.dump(lampValues, f)
-    f = open('lampValues.json')
-    lampValues = json.load(f)
     return render_template('index.html', element = json.load(open('lampValues.json'))['lamp']['sunrise'])
 
 if __name__ == '__main__':
